74a5818b4519a5bdae00ddcd9b5a2c4a156d6398
Haukur Kristinssin <haukzinn@gmail.com>
2017-01-21 22:24:34 +0100

    Changing card component

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -4,27 +4,18 @@ import FlatButton from 'material-ui/FlatButton';
 
 const CardComponent = () => (
   <Card>
-    <CardHeader
-      title="URL Avatar"
-      subtitle="Subtitle"
-      avatar="images/jsa-128.jpg"
-    />
     <CardMedia
-      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+      overlay={<CardTitle title="Hvad er enjam?"/>}
     >
       <img src="images/nature-600-337.jpg" />
     </CardMedia>
-    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardTitle title="enjam" />
     <CardText>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
       Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
       Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
     </CardText>
-    <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
   </Card>
 );
 
