996e030eff192c0a0b834522fc629effc3dea75e
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-02-03 18:38:33 +0100

    gamejam tekst -> pris og antal pladser

--- src/views/Jams/GameJam/GameJam.js
+++ src/views/Jams/GameJam/GameJam.js
@@ -14,7 +14,7 @@ const GameJam = (props) => (
 		<a href={eventLink}>
 			<img src={gamejam} className="event-image" role="presentation"/>
 		</a>
-		<p>Lav et spil i løbet af en weekend. Det kan være et brætspil, et computerspil eller et hvilket som helst andet slags spil. Kom fredag og find en gruppe, du vil lave et spil med. Vi kommer med et emne, og så har i fra fredag aften til at lave spillet. Søndag middag mødes vi alle sammen, prøver hinandens spil og stemmer om, hvem der skal have præmier i forskellige kategorier. Der vil være fede indslag i løbet af eventet, og vi sørger for en sandwich lørdag, så i ikke skal tænke på madpakken. Deltag i eventet på Facebook, så vi ved, i kommer, og ta jeres venner med. Det bliver fedt!</p>
+		<p>Lav et spil i løbet af en weekend. Det kan være et brætspil, et computerspil eller et hvilket som helst andet slags spil. Kom fredag og find en gruppe, du vil lave et spil med. Vi kommer med et emne, og så har i fra fredag aften til at lave spillet. Søndag middag mødes vi alle sammen, prøver hinandens spil og stemmer om, hvem der skal have præmier i forskellige kategorier. Der vil være fede indslag i løbet af eventet, og vi sørger for en sandwich lørdag, så i ikke skal tænke på madpakken. Arrangementet koster 30 kr. Der er 60 pladser. Det bliver fedt. Vi ses!</p>
 		<a href={eventLink}>
 			<EnButton title="TILMELD"/>
 		</a>
