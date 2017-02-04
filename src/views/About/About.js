5207885b5ef5f750efd8ded60ef4233a2789ba4a
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-02-04 22:37:01 +0100

    stavefejl facilitere

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = (props) => (
 	<div className="row left">
-		<p>Enjam arrangerer ingeniør-jams for studerende på SDU. Idéen er at skabe et kreativt miljø, der fasciliterer idéer udenfor rammerne af semesterprojekter og sætter de studerendes fagligheder i spil på tværs af uddannelserne, hvor alkohol ikke er omdrejningspunket.</p>
+		<p>Enjam arrangerer ingeniør-jams for studerende på SDU. Idéen er at skabe et kreativt miljø, der faciliterer idéer udenfor rammerne af semesterprojekter og sætter de studerendes fagligheder i spil på tværs af uddannelserne, hvor alkohol ikke er omdrejningspunket.</p>
 		<p>Hvert arrangement, hver jam, tager afsæt i et tema med udfordringer, der er relevante for en række ingeniørudannelser. Deltagerne finder sammen, gerne fra flere uddannelser, i mindre grupper omkring temaet.</p>
 		<p>Grupperne har weekenden til at lave et produkt, der er relateret til temaet. Som afslutning på arrangementet kan der være afstemning og præmier.</p>
 	</div>
