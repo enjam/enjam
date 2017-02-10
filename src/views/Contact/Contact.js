8a1d7aa9e49ed013f0a608a84518c701e5694940
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-02-10 14:35:17 +0100

    enjam mail

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = (props) => (
 	<div className="row">
-		<p>mail: rahau15@student.sdu.dk</p>
+		<p>mail: enjam@enjam.dk</p>
 		<p>mobil: 30 32 65 66</p>
 	</div>
 );
