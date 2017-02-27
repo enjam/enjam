27862521738154c3796644c8378e0ba3171edb30
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-02-27 23:23:59 +0100

    game text

--- src/views/Jams/GameJam/TrumpTris/TrumpTris.js
+++ src/views/Jams/GameJam/TrumpTris/TrumpTris.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router';
+
+import GameJam from '../GameJam';
 
 const style = {
 	width: '500px',
@@ -7,8 +10,12 @@ const style = {
 };
 
 const TrumpTris = () => (
-	<iframe src="http://trump.enjam.dk" style={style}>
-	</iframe>
+	<div className="row">
+		<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!"</p>
+		<iframe src="http://trump.enjam.dk" style={style}>
+		</iframe>
+		<GameJam />
+	</div>
 );
 
 export default TrumpTris;
