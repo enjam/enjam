26e642cdc31955abe3133d4894c645c2f362b727
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-02-27 23:48:10 +0100

    recommend using a computer on mobile

--- src/views/Jams/GameJam/TrumpTris/TrumpTris.js
+++ src/views/Jams/GameJam/TrumpTris/TrumpTris.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import mobile from 'is-mobile';
 
 import GameJam from '../GameJam';
 
@@ -9,14 +10,26 @@ const style = {
 	border: 'none'
 };
 
-const TrumpTris = () => (
-	<div className="row">
-		<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!"</p>
-		<iframe src="http://trump.enjam.dk" style={style}>
-		</iframe>
+const TrumpTris = () => {
+	let topContent;
+	if (mobile()){
+		topContent = <div>
+				<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!"</p>
+				<iframe src="http://trump.enjam.dk" style={style}>
+				</iframe>
+			</div>;
+	}else{
+		topContent = <div>
+			<p>Prøv spillet på en computer.</p>
+		</div>
+	}
+	
+	return <div className="row">
+		{topContent}
+		<p>(Virker bedst i Firefox og Chrome)</p>
 		<GameJam />
 	</div>
-);
+};
 
 export default TrumpTris;
 
