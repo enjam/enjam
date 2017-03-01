935c956e13ca665eab609e1f879b2d050046c9a9
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-03-01 19:13:22 +0100

    ny enjam beskrivelse

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = (props) => (
 	<div className="row left">
-		<p>Enjam arrangerer ingeniør-jams for studerende på SDU. Idéen er at skabe et kreativt miljø, der faciliterer idéer udenfor rammerne af semesterprojekter og sætter de studerendes fagligheder og interesser i spil på tværs af uddannelserne i et socialt arrangement.</p>
-		<p>Hver jam tager afsæt i et tema med udfordringer, der er relevante for en række ingeniørudannelser. Deltagerne finder sammen, gerne fra flere uddannelser, i mindre grupper omkring temaet.</p>
-		<p>Deltagerne har weekenden til at lave et produkt, og om søndagen skulle de gerne gå hjem med et produkt, eventuelle præmier, nye idéer og et større netværk.</p>
+		<p>Enjam arrangerer ingeniør-jams for studerende på SDU. Idéen er at skabe et kreativt miljø, der sætter de studerendes fagligheder og interesser i spil på tværs af uddannelserne.</p>
+		<p>Hver jam tager afsæt i et tema med udfordringer, der er relevante for en række ingeniørudannelser. Deltagerne finder sammen i mindre grupper og har weekenden til at lave et produkt.</p>
+		<p>Deltagerne skulle gerne tage hjem med , eventuelle præmier og et større netværk.</p>
 	</div>
 );
 
