a554096b52ecc34afbc14c962e321b0ba850a628
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-03-01 18:29:02 +0100

    tilføjet cvr

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Contact = (props) => (
 	<div className="row">
 		<p>mail: enjam@enjam.dk</p>
-		<p>mobil: 30 32 65 66</p>
+		<p>tlf: 30 32 65 66</p>
+		<p>cvr: 38420747</p>
 	</div>
 );
 
