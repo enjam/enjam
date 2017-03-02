5ad718477577a26db2490c59c6a74f2b714db2d2
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-03-02 11:33:40 +0100

    fix: manglede ord i tekst om enjam

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -4,7 +4,7 @@ const About = (props) => (
 	<div className="row left">
 		<p>Enjam arrangerer ingeniør-jams for studerende på SDU. Idéen er at skabe et kreativt miljø, der sætter de studerendes fagligheder og interesser i spil på tværs af uddannelserne.</p>
 		<p>Hver jam tager afsæt i et tema med udfordringer, der er relevante for en række ingeniørudannelser. Deltagerne finder sammen i mindre grupper og har weekenden til at lave et produkt.</p>
-		<p>Deltagerne skulle gerne tage hjem med , eventuelle præmier og et større netværk.</p>
+		<p>Deltagerne skulle gerne tage hjem med inspiration, eventuelle præmier og et større netværk.</p>
 	</div>
 );
 
