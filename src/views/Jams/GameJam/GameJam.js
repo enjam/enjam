0e83baf7a2d93acecea6db2cc2e1c9f2f6c61e89
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-03-03 00:17:40 +0100

    new gamejam cover - updated text - link from game to fb

--- src/views/Jams/GameJam/GameJam.js
+++ src/views/Jams/GameJam/GameJam.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
-import gamejam from './gamejam.png';
+import {Link} from 'react-router';
+import gamejam from './game_logo.png';
 import EnButton from '../../../components/EnButton/EnButton';
 
 const eventLink = "https://www.facebook.com/events/805414926263448/";
+
+const img_style = {
+	maxWidth: '300px',
+}
 
 const GameJam = (props) => (
 	<div className="row">
@@ -11,10 +15,11 @@ const GameJam = (props) => (
 			<h1 className="event-header">Gamejam</h1>
 		</a>
 		<h3>24.-26. marts</h3>
-		<a href={eventLink}>
-			<img src={gamejam} className="event-image" role="presentation"/>
-		</a>
-		<p>Lav et spil i løbet af en weekend. Det kan være et brætspil, et computerspil eller et hvilket som helst andet slags spil. Kom fredag og find en gruppe, du vil lave et spil med. Vi kommer med et emne, og så har i fra fredag aften til at lave spillet. Søndag middag mødes vi alle sammen, prøver hinandens spil og stemmer om, hvem der skal have præmier i forskellige kategorier. Der vil være fede indslag i løbet af eventet, og vi sørger for en sandwich lørdag, så i ikke skal tænke på madpakken. Arrangementet koster 30 kr. Der er 60 pladser. Det bliver fedt. Vi ses!</p>
+		<p>Lav et spil i løbet af en weekend. Det kan være et brætspil, et computerspil eller et hvilket som helst andet slags spil. Kom fredag og find en gruppe, du vil lave et spil med. Vi kommer med et emne, og så har i fra fredag aften til at lave spillet. Søndag eftermiddag viser alle deres spil og stemmer om, hvem der skal have præmier i forskellige kategorier. Der vil være fede indslag i løbet af eventet, og vi sørger for noget mad, så i ikke skal tænke på madpakken. Arrangementet koster 30 kr. Der er 60 pladser. Læs mere og tilmeld jer via linket. Vi ses!</p>
+		<Link to="/trump">
+			<img src={gamejam} className="event-image" role="presentation" style={img_style}/>
+		</Link>
+		<br/>
 		<a href={eventLink}>
 			<EnButton title="Tilmeld"/>
 		</a>
--- src/views/Jams/GameJam/TrumpTris/TrumpTris.js
+++ src/views/Jams/GameJam/TrumpTris/TrumpTris.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router';
 import mobile from 'is-mobile';
 
 import GameJam from '../GameJam';
+import EnButton from '../../../../components/EnButton/EnButton';
+
+const eventLink = "https://www.facebook.com/events/805414926263448/";
 
 const style = {
 	width: '500px',
@@ -12,24 +15,21 @@ const style = {
 };
 
 const TrumpTris = () => {
-	let topContent;
 	if (!mobile()){
-		topContent = <div>
-				<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!"</p>
-				<iframe src="http://trump.enjam.dk" style={style} scrolling="no">
-				</iframe>
-			</div>;
+		return <div>
+			<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!" <br/> (Virker bedst i Firefox og Chrome)</p>
+			<iframe src="http://trump.enjam.dk" style={style} scrolling="no" />
+			<p>Se mere og tilmeld dig via linket. Vi ses!</p>
+			<a href={eventLink}>
+				<EnButton title="Tilmeld"/>
+			</a>
+		</div>;
 	}else{
-		topContent = <div>
-			<p>Prøv spillet på en computer.</p>
+		return <div>
+			<p>Du kan desværre kun prøve spillet på en computer.</p>
+			<GameJam />
 		</div>
 	}
-	
-	return <div className="row">
-		{topContent}
-		<p>(Virker bedst i Firefox og Chrome)</p>
-		<GameJam />
-	</div>
 };
 
 export default TrumpTris;
