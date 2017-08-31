aebfc5e70bf3b8ee74a2542d59978dc44af3a10c
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-08-31 15:36:29 +0200

    trump http -> https

--- src/views/Jams/GameJam/TrumpTris/TrumpTris.js
+++ src/views/Jams/GameJam/TrumpTris/TrumpTris.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import mobile from 'is-mobile';
 
-import GameJam from '../GameJam';
+import EnPage from '../../../../components/EnPage';
+import GameJamPage from '../GameJamPage';
 import RaisedButton from 'material-ui/RaisedButton';
+
+import {Link} from 'react-router';
 
 const eventLink = "https://www.facebook.com/events/805414926263448/";
 
@@ -17,20 +20,23 @@ const TrumpTris = () => {
 	if (!mobile()){
 		return <div style={{marginBottom: '20px', textAlign: 'center'}}>
 			<p>Trump: "Build a wall with 200 points in 2 minutes or you're fired!" <br/> (Virker bedst i Firefox og Chrome)</p>
-			<iframe src="http://trump.enjam.dk" style={style} scrolling="no" />
-			<p>Se mere og tilmeld dig via linket. Vi ses!</p>
+			<iframe src="https://trump.enjam.dk" style={style} scrolling="no" />
+			<p>
+				Spillet er lavet som reklame for Gamejammet i foråret 2017.<br/>
+				Du kan se mere om jammet <Link to="/gamejam">her</Link> og på facebook.
+			</p>
 			<a href={eventLink} target="_blank">
-				<RaisedButton label="Tilmeld 30 kr" primary={true} />
+				<RaisedButton label="Facebook" primary={true} />
 			</a>
 		</div>;
 	}else{
 		return <div>
-			<p>Du kan desværre kun prøve spillet på en computer.</p>
-			<GameJam />
+			<EnPage>
+				<p>Du kan desværre kun prøve spillet på en computer.</p>
+			</EnPage>
+			<GameJamPage/>
 		</div>
 	}
 };
 
 export default TrumpTris;
-
-
